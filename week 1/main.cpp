72244eab854bf7de334fc0f43472a94fe9858934
maokuanzhang <maokuan.zhang@hotmail.com>
2025-08-19 21:12:05 -0700

    Create main.cpp for week 1 assignment

---
+++ week 1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello Inventory System!\n";
+    cout << "\tVariable Sizes:\n";
+    cout << "Size of int:   " << sizeof(int) << " bytes\n";
+    cout << "Size of float: " << sizeof(float) << " bytes\n";
+    cout << "Size of char:  " << sizeof(char) << " bytes\n";
+    return 0;
+}
