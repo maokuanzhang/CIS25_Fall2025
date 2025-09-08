25ae904d642ee91e28f68409d58ae9893d51ac11
maokuanzhang <maokuan.zhang@hotmail.com>
2025-09-08 12:09:19 -0700

    Add Week 3 Assignment (main.cpp + shopping.h)

---
+++ shopping.h
@@ -0,0 +1,13 @@
+// shopping.h
+// Author: Maokuan Zhang
+// Course: CIS25 Fall 2025
+// Assignment: Week 3
+// Description: Header files for Module 3 program
+
+#ifndef SHOPPING_H
+#define SHOPPING_H
+
+#include <iostream>
+#include <string>
+
+#endif
