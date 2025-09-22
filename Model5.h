6ce5b87f7f39fdc57000992bd98864cb7a3a2f73
maokuanzhang <maokuan.zhang@hotmail.com>
2025-09-21 20:25:19 -0700

    Add files via upload

---
+++ Model5.h
@@ -0,0 +1,11 @@
+// Model5.h
+// Contains function declarations
+#ifndef MODEL5_H
+#define MODEL5_H
+
+void showMenu();
+int add(int a,int b);
+int multiply(int a, int b);
+int square(int a);
+
+#endif
